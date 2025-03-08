50e6ba83c6c873bc1465e6bd3fe189092bacbec0
VioRush <violetta.rushnitskaya@gmail.com>
2025-03-08 14:39:18 +0100

    Initial commit

---
+++ src/test/java/com/leverx/training/RatingSystem/RatingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leverx.training.RatingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
